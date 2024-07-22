2999d4639e22a6d4f845da837deb648808ab6d99
Dixin Shen <dixinshen@gmail.com>
2024-07-21 23:42:49 -0700

    Update computation time 2
    set seed

--- computation_time.R
+++ computation_time.R
@@ -1,3 +1,5 @@
+set.seed(2024)
+
 shape <- 8
 scale <- 5
 N = 100
@@ -6,7 +8,10 @@ q = 50
 a <- rep(0, q)
 a[1:10] <- 0.2
 
-for (p in c(200, 1000, 10000)) {
+compTime <- data.frame(p200=rep(NA,100), p1000=rep(NA,100), p10000=rep(NA,100))
+nFeature <- c(200, 1000, 10000)
+for (ind in 1:3) {
+    p <- nFeature[ind]
     z <- rbinom(p*q, size = 1, prob = 0.1)
     z <- matrix(z, nrow = p, ncol = q)
     mode(z) <- "double"
@@ -35,50 +40,32 @@ for (p in c(200, 1000, 10000)) {
         if ( abs(Cindex(drop(x%*%b), y) - 0.8) < 0.001 ) break
     }
     
-    x <- mvnfast::rmvn(N, rep(0, p), sigma)
-    t <- scale * (-log(runif(N)) * exp(-drop(x%*%b)))^(1/shape) + rnorm(N, sd = stdev)
-    t[t<=0] <- min(t[t>0])
-    t[t>20] <- 20    # follow up time is 20
-    c <- rexp(N, 0.06)     # produce ratio of event to censor at around 7/3
-    c[c>20] <- 20
-    time <- pmin(t,c)
-    status <- ifelse(t<c, 1, 0)
-    table(status)
-    y <- cbind(time, status)
-    
-    ordered <- order(y[,1])
-    y <- y[ordered, ]
-    x <- as.matrix(x[ordered, ])
-    
-    start <- Sys.time()
-    for (j in 1:100) {
+    for (rep in 1:100) {
+        x <- mvnfast::rmvn(N, rep(0, p), sigma)
+        t <- scale * (-log(runif(N)) * exp(-drop(x%*%b)))^(1/shape) + rnorm(N, sd = stdev)
+        t[t<=0] <- min(t[t>0])
+        t[t>20] <- 20    # follow up time is 20
+        c <- rexp(N, 0.06)     # produce ratio of event to censor at around 7/3
+        c[c>20] <- 20
+        time <- pmin(t,c)
+        status <- ifelse(t<c, 1, 0)
+        table(status)
+        y <- cbind(time, status)
+        
+        ordered <- order(y[,1])
+        y <- y[ordered, ]
+        x <- as.matrix(x[ordered, ])
+        
+        start <- Sys.time()
         fit_r_l <- xrnet(x, y, z, family="cox", 
                          penalty_main=define_ridge(penalty_ratio=0.01), 
                          penalty_external=define_lasso(penalty_ratio=0.01),
                          control=list(max_iterations=1e6))
+        end <- Sys.time()
+        compTime[rep, ind] <- difftime(end, start, units = "secs")
     }
-    end <- Sys.time()
-    cat("The average computational time of N=100, p=", p, ", q=50, is ", (end-start)/100, "\n")
+    
+    cat("The average computational time of N=100, p=", nFeature[ind], ", q=50, is ", mean(compTime[,ind]), "seconds.\n")
 }
-
-start <- Sys.time()
-fit_r_l <- xrnet(x, y, z, family="cox", 
-                 penalty_main=define_ridge(penalty_ratio=0.01), 
-                 penalty_external=define_lasso(penalty_ratio=0.01),
-                 control=list(max_iterations=1e6))
-end <- Sys.time()
-end - start
-
-
-
-
-
-
-
-
-
-
-
-
 
 
